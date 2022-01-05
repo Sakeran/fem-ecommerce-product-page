0d1ec57251604344b24667660be4f05385d4e614
Alexander <Sakeran@gmail.com>
2022-01-05 17:33:57 -0500

    Set up typography for TailwindCSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,21 @@
 module.exports = {
   content: ["src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    // Overrides
+    fontFamily: {
+      sans: ["Kumbh Sans", "sans-serif"],
+    },
+
+    // Extensions
+    extend: {
+      fontSize: {
+        tiny: ["0.8125rem", "1"],
+        sm: ["0.9375rem", "1.66"],
+        base: ["1rem", "1.625"],
+        "2.5xl": ["1.75rem", "1.15"],
+        "4.5xl": ["2.75rem", "1.1"],
+      },
+    },
   },
   plugins: [],
-}
+};
