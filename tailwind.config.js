926c5ab0f1a8928a55fb5fb5654fb16a9542d1b0
Alexander <Sakeran@gmail.com>
2022-01-05 18:28:02 -0500

    Add colors to TailwindCSS config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,22 @@ module.exports = {
     fontFamily: {
       sans: ["Kumbh Sans", "sans-serif"],
     },
+    colors: {
+      // Neutrals
+      white: "hsl(0, 0%, 100%)",
+      black: "hsl(0, 0%, 0%)",
+      gray: {
+        100: "hsl(220, 14%, 13%)",
+        400: "hsl(220, 9%, 45%)",
+        700: "hsl(220, 14%, 75)",
+      },
+      // Oranges
+      orange: {
+        500: "hsl(26, 100%, 55%)",
+        700: "hsl(26, 100%, 71%)",
+        900: "hsl(26, 100%, 94%)"
+      }
+    },
 
     // Extensions
     extend: {
