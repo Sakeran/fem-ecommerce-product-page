3d5f38b98c575fb14b416d03fccd335d1d33dfcb
Alexander <Sakeran@gmail.com>
2022-01-05 14:43:56 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import type { Component } from 'solid-js';
+
+const App: Component = () => {
+  return (
+    <div className="">
+      FEM E-Commerce Product Page
+    </div>
+  );
+};
+
+export default App;
