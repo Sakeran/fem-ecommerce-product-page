46d0c4c551ec2f556d41768e6028ec0c345c1bfd
Alexander <Sakeran@gmail.com>
2022-01-07 14:21:18 -0500

    Style Header for desktop

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,23 +9,21 @@ export default function Header() {
   const toggleMenu = () => setExpanded((exp) => !exp);
 
   return (
-    <header class="relative pt-5 pb-7 px-6 flex items-center gap-4">
-        <button
-          aria-controls="primary-nav"
-          aria-expanded={expanded()}
-          onClick={toggleMenu}
-          class="translate-y-[2px]"
-        >
-          <img src={menuIcon} alt="" aria-hidden="true" />
-          <span class="sr-only">Toggle Menu</span>
-        </button>
-      {/* <div>
-      </div> */}
+    <header class="wrapper relative pt-5 pb-7 px-6 flex items-center gap-4 md:gap-14 sm:border-b sm:border-b-gray-900 sm:py-0">
+      <button
+        aria-controls="primary-nav"
+        aria-expanded={expanded()}
+        onClick={toggleMenu}
+        class="translate-y-[2px] sm:hidden"
+      >
+        <img src={menuIcon} alt="" aria-hidden="true" />
+        <span class="sr-only">Toggle Menu</span>
+      </button>
       <div>
         <img src="/images/logo.svg" alt="" />
       </div>
       <div
-        class="fixed bg-black/75 inset-0 transition-opacity duration-300 ease-in-out"
+        class="fixed bg-black/75 inset-0 transition-opacity duration-300 ease-in-out sm:hidden sm:pointer-events-none"
         classList={{
           "opacity-100": expanded(),
           "opacity-0": !expanded(),
@@ -34,20 +32,19 @@ export default function Header() {
       ></div>
       <nav
         id="primary-nav"
-        class="z-50 fixed left-0 top-0 bottom-0 bg-white min-w-[250px] p-6 flex flex-col gap-[3.25rem] transition-transform duration-200 ease-in-out"
+        class="z-50 fixed left-0 top-0 bottom-0 bg-white min-w-[250px] p-6 flex flex-col gap-[3.25rem] transition-transform duration-200 ease-in-out sm:bg-none sm:static sm:translate-x-0 sm:p-0 sm:pt-10 sm:gap-8"
         classList={{ ["-translate-x-full"]: !expanded() }}
       >
-        <div>
-          <button
-            aria-controls="primary-nav"
-            aria-expanded={expanded()}
-            onClick={toggleMenu}
-          >
-            <img src={closeIcon} alt="" aria-hidden="true" />
-            <span class="sr-only">Close Menu</span>
-          </button>
-        </div>
-        <ul class="flex flex-col gap-5">
+        <button
+          aria-controls="primary-nav"
+          aria-expanded={expanded()}
+          onClick={toggleMenu}
+          class="sm:hidden"
+        >
+          <img src={closeIcon} alt="" aria-hidden="true" />
+          <span class="sr-only">Close Menu</span>
+        </button>
+        <ul class="flex flex-col gap-5 sm:flex-row md:gap-8">
           <NavItem text="Collections" />
           <NavItem text="Men" />
           <NavItem text="Women" />
@@ -66,8 +63,13 @@ const NavItem: Component<{ text: string; href?: string }> = (props) => {
   const itemProps = mergeProps({ href: "#" }, props);
 
   return (
-    <li class="text-lg text-gray-100 font-bold">
-      <a href={itemProps.href || "#"}>{itemProps.text}</a>
+    <li class="sm:flex">
+      <a
+        class="text-lg text-gray-100 font-bold sm:text-sm sm:font-normal sm:text-gray-400 sm:hover:text-gray-100 sm:pb-11 sm:border-b-2 sm:border-orange-500/0 sm:hover:border-orange-500 sm:focus:border-orange-500"
+        href={itemProps.href || "#"}
+      >
+        {itemProps.text}
+      </a>
     </li>
   );
 };
--- src/components/HeaderUserInfo.tsx
+++ src/components/HeaderUserInfo.tsx
@@ -10,7 +10,7 @@ export default function HeaderUserInfo() {
   const toggleCart = () => setCartVisible((v) => !v);
 
   return (
-    <div class="flex items-center gap-5">
+    <div class="flex items-center gap-5 md:gap-11">
       <div class="contents">
         <button
           aria-controls="user-cart"
@@ -31,14 +31,14 @@ export default function HeaderUserInfo() {
           <Cart />
         </div>
       </div>
-      <div class="relative w-6 aspect-square rounded-full overflow-hidden">
+      <button class="relative w-6 aspect-square rounded-full overflow-hidden sm:w-[3.125rem] sm:border-2 sm:border-orange-500/0 sm:hover:border-orange-500 transition-colors">
         <Show
           when={currentUser().avatarImg}
           fallback={<div class="absolute bg-gray-400 inset-0"></div>}
         >
           <img src={currentUser().avatarImg} alt="User avatar" />
         </Show>
-      </div>
+      </button>
     </div>
   );
 }
